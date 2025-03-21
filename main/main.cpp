e1821fa5ebb987b4696903f4e0b8ccfd082d68a0
Kevin Perillo <mootseeker98@gmail.com>
2025-03-21 18:20:34 +0100

    Add basic initialization of flash memory and created default loop.

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,31 @@
 
 #include <cstdio>
 
-#include "../application/app.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-App::Application MyApp; 
+#include "esp_event.h"
+#include "esp_log.h"
 
+#include "nvs_flash.h"
+
+static const char* TAG = "MAIN"; 
 
 extern "C" void app_main(void)
 {
+    // Initialize Non-Volatile storgae (NVS)
+    esp_err_t state = nvs_flash_init( ); 
+    if( state == ESP_ERR_NVS_NO_FREE_PAGES || 
+        state == ESP_ERR_NVS_NEW_VERSION_FOUND )
+    {
+        ESP_ERROR_CHECK( nvs_flash_erase( )); 
+        ESP_ERROR_CHECK( nvs_flash_init( )); 
+    }
 
+    ESP_ERROR_CHECK( state ); 
+
+    // Initialize the event loop system
+    ESP_ERROR_CHECK( esp_event_loop_create_default( )); 
+
+    ESP_LOGI( TAG, "System initialized"); 
 }
