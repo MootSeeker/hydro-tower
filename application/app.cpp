0e744ecb2208bf44d6bd08704b2297d35980d3ff
Kevin Perillo <mootseeker98@gmail.com>
2025-03-21 19:16:14 +0100

    Implement basic event Task.

--- application/app.cpp
+++ application/app.cpp
@@ -1,10 +1,93 @@
 
 #include "app.h"
 
+#include "esp_log.h"
+
 namespace App
 {
-    Application::Application( void )
-    {
+    static const char* TAG = "APP"; 
 
+    Application::Application( void ) : _eventTaskHandle( nullptr )
+    {
+        _eventQueue = xQueueCreate( 10, sizeof( EventMessage )); 
+    }
+
+    Application& Application::GetInstance( void )
+    {
+        static Application instance; 
+        return instance; 
+    }
+
+    void Application::Start( void )
+    {
+        BaseType_t state = pdFALSE; 
+
+        ESP_LOGI( TAG, "Starting application..." ); 
+
+        state = xTaskCreatePinnedToCore(    eventHandlerTask, 
+                                            "Event Handler", 
+                                            4096, 
+                                            nullptr, 
+                                            2, 
+                                            &_eventTaskHandle, 
+                                            1 ); 
+        ESP_ERROR_CHECK( state );
+    }
+
+    void Application::PostEvent( AppEvent event, void *data )
+    {
+        BaseType_t state = pdFALSE;
+
+        EventMessage msg = { event, data }; 
+        state = xQueueSend( _eventQueue, &msg, portMAX_DELAY ); 
+
+        ESP_ERROR_CHECK( state ); 
+    }
+
+    void Application::eventHandlerTask( void* args )
+    {
+        ESP_LOGI( TAG, "Event Handler Task started on Core %d", xPortGetCoreID( ));
+
+        EventMessage msg; 
+
+        for( ;; )
+        {
+            if( xQueueReceive( GetInstance( )._eventQueue, &msg, portMAX_DELAY ))
+            {
+                switch( msg.event )
+                {
+                    case AppEvent::SENSOR_UPDATE: 
+                    {
+                        ESP_LOGI( TAG, "Processing SENSOR_UPDATE event" );
+                        break; 
+                    }
+
+                    case AppEvent::WIFI_CONNECTED: 
+                    {
+                        ESP_LOGI( TAG, "Processing WIFI_CONNECTED event" );
+                        break; 
+                    }
+
+                    case AppEvent::WIDI_DISCONNECTED: 
+                    {
+                        ESP_LOGI( TAG, "Processing WIFI_DISCONNECTED event" );
+                        break; 
+                    }
+
+                    case AppEvent::UI_REFRESH: 
+                    {
+                        ESP_LOGI( TAG, "Processing UI_REFRESH event" );
+                        break; 
+                    }
+
+                    case AppEvent::NONE: 
+                    default: 
+                    {
+                        ESP_LOGI( TAG, "Received wrong Event - do nothing" );
+                        break; 
+                    }
+                }
+            }
+        }
     }
 }
--- application/app.h
+++ application/app.h
@@ -2,17 +2,41 @@
 #ifndef APP_H
 #define APP_H
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
 namespace App
 {
+
+    enum class AppEvent 
+    {
+        NONE, 
+        SENSOR_UPDATE, 
+        WIFI_CONNECTED, 
+        WIDI_DISCONNECTED, 
+        UI_REFRESH
+    }; 
+
+    struct EventMessage
+    {
+        AppEvent event; 
+        void *data; 
+    }; 
+
     class Application
     {
-        public: 
-            Application( void ); 
+        public:
+            static Application& GetInstance( void );  
+            void Start( void ); 
+            void PostEvent( AppEvent event, void *data = nullptr ); 
 
-    
         private: 
+            Application( void ); 
+            static void eventHandlerTask( void* arg ); 
     
-    
+            TaskHandle_t _eventTaskHandle; 
+            QueueHandle_t _eventQueue; 
     
     }; // End: class Application    
 } // End: namespace App
